02a930da35cd51ee16f28231fbb2531a2034dea5
Adéla Přibylová <adelpribyl@gmail.com>
2024-05-31 14:30:47 +0200

    Řešení obou cvičení

--- src/components/Star/index.jsx
+++ src/components/Star/index.jsx
@@ -1,14 +1,15 @@
 import { useState } from 'react';
 import './style.css';
 
-export const Star = () => {
-  const [glowing, setGlowing] = useState(false);
-  
-  const starClass = glowing
-    ? 'rating__star rating__star--on'
-    : 'rating__star';
+export const Star = ({ glowing, value, onSelect }) => {
+  const starClass = glowing ? 'rating__star rating__star--on' : 'rating__star';
 
   return (
-    <div className={starClass}></div>
+    <div
+      className={starClass}
+      onClick={() => {
+        onSelect(value);
+      }}
+    ></div>
   );
 };
